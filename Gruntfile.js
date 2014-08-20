ab477698678599e72105293ac2e12fef5b87b8d0
Basil Veerman <bveerman@uvic.ca>
2014-08-20 16:09:04 -0700

    Remove grunt-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,20 +62,12 @@ module.exports = function(grunt) {
                 src: 'dist/<%= pkg.name %>.js',
                 dest: 'dist/<%= pkg.name %>.<%= pkg.version %>.min.js'
             }
-        },
-
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.files %>',
-                tasks: ['jshint']
-            }
-        },
+        }
     });
 
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('unittest', 'mochaTest');
