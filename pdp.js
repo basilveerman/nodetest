9a6cca62ca01b2aaed7b1a9239490961ef5ad2fa
Basil Veerman <bveerman@uvic.ca>
2014-08-20 13:19:02 -0700

    Add cftime module with tests

--- pdp.js
+++ pdp.js
@@ -2,5 +2,6 @@ module.exports = {
     dom: require('./lib/dom.js'),
     auth: require('./lib/auth.js'),
     raster: require('./lib/raster.js'),
-    vector: require('./lib/vector.js')
+    vector: require('./lib/vector.js'),
+    cftime: require('./lib/cftime.js')
 };
